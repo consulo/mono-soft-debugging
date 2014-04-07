784fd89230c823adeb6076c8a20c9ca773939558
VISTALL <vistall.valeriy@gmail.com>
2014-04-07 14:55:32 +0300

    initial

---
+++ testSrc/test/Main.java
@@ -0,0 +1,28 @@
+package test;
+
+import java.util.Map;
+
+import mono.debugger.SocketListeningConnector;
+import mono.debugger.VirtualMachine;
+import mono.debugger.connect.Connector;
+
+/**
+ * @author VISTALL
+ * @since 07.04.14
+ */
+public class Main
+{
+	public static void main(String[] args) throws Exception
+	{
+		SocketListeningConnector socketListeningConnector = new SocketListeningConnector();
+
+		Map<String,Connector.Argument> argumentMap = socketListeningConnector.defaultArguments();
+
+		argumentMap.get(SocketListeningConnector.ARG_LOCALADDR).setValue("127.0.0.1");
+		argumentMap.get(SocketListeningConnector.ARG_PORT).setValue("10110");
+
+		VirtualMachine accept = socketListeningConnector.accept(argumentMap);
+
+		System.out.println("virtualMachine connected " + accept);
+	}
+}
---
+++ testTarget/start.bat
@@ -0,0 +1,3 @@
+mono --debug --debugger-agent=transport=dt_socket,address=127.0.0.1:10110,suspend=y,server=n XDotNetTest.exe
+
+pause
