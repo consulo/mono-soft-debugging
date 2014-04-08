46536fe4abc10bfb95f9187a06ab53326211bc22
VISTALL <vistall.valeriy@gmail.com>
2014-04-08 14:35:59 +0300

    removed unused code

--- testSrc/test/Main.java
+++ testSrc/test/Main.java
@@ -23,16 +23,17 @@ public class Main
 
 		VirtualMachine accept = socketListeningConnector.accept(argumentMap);
 
-		accept.suspend();
+		accept.resume();
 
 		System.out.println(accept.description());
 
-		accept.resume();
-
 		System.out.println("waiting 10 sec and suspend");
+
 		Thread.sleep(10000L);
 
 		System.out.println("try to pause");
+
+		accept.suspend();
 
 		Thread.sleep(50000L);
 	}
