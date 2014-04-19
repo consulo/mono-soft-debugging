7cc0dbe07cc68236b1c8a1b313145d6a26898f49
VISTALL <vistall.valeriy@gmail.com>
2014-04-19 11:25:47 +0300

    support generic

---
+++ src/mono/debugger/GenericTarget.java
@@ -0,0 +1,20 @@
+package mono.debugger;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @author VISTALL
+ * @since 19.04.14
+ */
+public interface GenericTarget<T extends Mirror>
+{
+	@Nullable("Null is this is original class, if it a runtime copy with generic - ill return not null")
+	T original();
+
+	@NotNull
+	String originalName();
+
+	@NotNull
+	TypeMirror[] genericArguments();
+}
--- src/mono/debugger/protocol/Type_GetInfo.java
+++ src/mono/debugger/protocol/Type_GetInfo.java
@@ -39,6 +39,8 @@ public class Type_GetInfo implements Type
 	public final String fullName;
 	public final AssemblyMirror assemblyMirror;
 	public final TypeMirror baseType;
+	public TypeMirror generalType;
+	public TypeMirror[] genericArguments = TypeMirror.EMPTY_ARRAY;
 
 	private Type_GetInfo(VirtualMachineImpl vm, PacketStream ps)
 	{
@@ -48,5 +50,40 @@ public class Type_GetInfo implements Type
 		assemblyMirror = ps.readAssemblyMirror();
 		ps.readId(); //TODO [VISTALL] ModuleMirror
 		baseType = ps.readTypeMirror();
+		TypeMirror elementType = ps.readTypeMirror();
+		int token = ps.readInt();
+		int rank = ps.readByte();
+		int attributes = ps.readInt();
+		byte runtimeAttributes = ps.readByte();
+
+		boolean is_byref = (runtimeAttributes & 1) != 0;
+		boolean is_pointer = (runtimeAttributes & 2) != 0;
+		boolean is_primitive = (runtimeAttributes & 4) != 0;
+		boolean is_valuetype = (runtimeAttributes & 8) != 0;
+		boolean is_enum = (runtimeAttributes & 16) != 0;
+		boolean is_gtd = (runtimeAttributes & 32) != 0;
+		boolean is_generic_type = (runtimeAttributes & 64) != 0;
+
+		int nestedTypesSize = ps.readInt();
+		TypeMirror[] nestedTypes = new TypeMirror[nestedTypesSize];
+		for(int i = 0; i < nestedTypesSize; i++)
+		{
+			nestedTypes[i] = ps.readTypeMirror();
+		}
+
+		if(vm.isAtLeastVersion(2, 12))
+		{
+			generalType = ps.readTypeMirror();
+		}
+
+		if(vm.isAtLeastVersion(2, 15) && is_generic_type)
+		{
+			int n = ps.readInt();
+			genericArguments = new TypeMirror[n];
+			for(int i = 0; i < n; i++)
+			{
+				genericArguments[i] = ps.readTypeMirror();
+			}
+		}
 	}
 }
