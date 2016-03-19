0ce6396e225899095308345f3005e63fb3edb94b
VISTALL <vistall.valeriy@gmail.com>
2016-03-19 11:50:42 +0300

    wrong code style

---
+++ testSrc/test/AA.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @author VISTALL
+ * @since 23.07.2015
+ */
+public class AA
+{
+	public static void main(String[] args)
+	{
+		int b = 0xfffffff1 & 0xFF;
+		System.out.println(b);
+	}
+}
